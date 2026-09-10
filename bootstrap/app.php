06c036f93f5bb7348b77f45ecbd972f20661f403
memo <l22410394@itparral.edu.mx>
2026-09-09 20:39:35 -0600

    feat: Middleware de entrada con validacion de cabecera y rol

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,15 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__.'/../console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // Registramos el alias aquí
+        $middleware->alias([
+            'clave.acceso' => \App\Http\Middleware\ValidarClaveAcceso::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/area-protegida', function () {
+    return response()->json(['mensaje' => '¡Bienvenido al área protegida con éxito!']);
+})->middleware('clave.acceso');
+//---------------------------
 Route::get('/', function () {
     return view('welcome');
 });
